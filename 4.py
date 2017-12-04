4a5833f272bafde2cc40669bea9de8e855ab3f09
Stephen Johnson <stephen.johnson@exptechinc.com>
2017-12-04 08:42:25 -0500

    solutions to 4

---
+++ 4.py
@@ -0,0 +1,45 @@
+"""Solutions for AOC Day 3"""
+import math
+import numpy
+import itertools
+
+def good_pass_1(passt):
+    words = passt.split()
+    set_words = set(words)
+    if len(words) == len(set_words):
+        return True
+    return False
+
+def good_pass_2(passt):
+    words = passt.split()
+    set_words = set(words)
+    if len(words) != len(set_words):
+        return False
+    for pair in itertools.combinations(words, 2):
+        if sorted(pair[0]) == sorted(pair[1]):
+            return False
+    return True
+
+def sol_1():
+    """Solution to #1"""
+    good = 0
+    for line in open("4.txt"):
+        if good_pass_1(line):
+            good += 1
+    
+    return good
+
+
+def sol_2():
+    """Solution to #1"""
+    good = 0
+    for line in open("4.txt"):
+        if good_pass_2(line):
+            good += 1
+    
+    return good
+
+
+if __name__ == "__main__":
+    print sol_1()
+    print sol_2()
