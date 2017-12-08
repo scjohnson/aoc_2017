c33913961b2b31ad3c9bbe8515a11c1428ae1ed5
Stephen Johnson <stephen.johnson@exptechinc.com>
2017-12-08 08:09:43 -0500

    small cleanup

--- 7.py
+++ 7.py
@@ -29,7 +29,7 @@ def create_tree(file_name):
             for v in value:
                 pointed_to[v] = True
     for key in pointed_to:
-        if pointed_to[key] == False:
+        if not pointed_to[key]:
             return key, tree, weights
 
 
@@ -41,12 +41,15 @@ def calc_weight(base, tree, weights):
     leaf_dict = {}
     for leaf in leaves:
         leaf_weight = calc_weight(leaf, tree, weights)
+        if not leaf_weight:
+            return False
         leaf_weights.append(leaf_weight)
         leaf_dict[leaf] = leaf_weight
     if len(set(leaf_weights)) != 1:
         print leaf_dict
         for leaf in leaves:
             print leaf, weights[leaf]
+        return False
     return sum(leaf_weights) + weights[base]
 
 
@@ -54,5 +57,4 @@ if __name__ == "__main__":
     base, tree, weights = create_tree("7.txt")
     print "base = ", base
     calc_weight(base, tree, weights)
-    # base = sol_1() #wiapj
     # print sol_2(base) #1072
